5d7107ad9fd74d997dc9b8833505d68538a08f1d
Jose Von Chong <javch1995@gmail.com>
2022-04-07 23:56:07 -0500

    Reactividad - count var added

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,6 +1,11 @@
 <script>
     let description =
         "Grupo de golf de Panama dedicado a la perfeccion del golf";
+    let count = 0;
+    function clickHandle() {
+        count += 1;
+        console.log(count);
+    }
 </script>
 
 <main>
@@ -9,6 +14,8 @@
             {description}
         </p>
     </div>
+
+    <button on:click={clickHandle}>Click {count === 0 ? "" : count}</button>
 </main>
 
 <style>
