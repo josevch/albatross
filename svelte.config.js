bf46de23110f8c000ca003b0f086d5451d4ccf0c
Jose Von Chong <jvonchong@copaair.com>
2023-02-11 14:18:32 -0500

    Real stuff begins

---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
+};
+
+export default config;
